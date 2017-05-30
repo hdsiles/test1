b353d0c076fdffc0e17196e4483e7696c67f3d7b
rackerroush <justin.roush@rackspace.com>
2017-05-30 13:39:00 -0500

    Set version to 1.0.2

--- update_version.sh
+++ update_version.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-VER=$1
-echo "Updating to version $VER"
-echo $VER > version
+VERSION=$1
+
+# Test for correct x.x.x semantic version for input
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]];
+then
+    VERSION=${BASH_REMATCH[0]};
+    echo "INFO: Setting to version ${VERSION}"
+else
+    echo "
+    Not able to parse version from input.
+    Usage example: ${BASH_SOURCE[0]} 0.1.2
+    "
+    exit 0
+fi
+
+echo $VERSION > version
 git add update_version.sh
 git add version
-git commit -m "Set version to $VER"
+git commit -m "Set version to $VERSION"
 git push origin
 
