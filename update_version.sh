7dc501763339df13d85388eb700f9e3436c1b567
rackerroush <justin.roush@rackspace.com>
2017-05-31 22:42:43 -0500

    added commits to PR body

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION=$1
 REPOSITORY='test1'
 OWNER='rackerroush'
@@ -18,27 +20,28 @@ else
     exit 0
 fi
 
+echo
+echo When creating the PR into master, here are the commit messages since the last version change:
+echo
+CHG_LOG=()
+#git --no-pager log --no-merges --format=%s | awk '/^Set version to '"${VERSION}"'$/{flag=1;next}/^Set version to [0-9]+\.[0-9]+\.[0-9]+$/{flag=0}flag' | 
+while read line
+do
+    echo "- $line"
+    CHG_LOG+=("$line")
+    #CHG_LOG="$CHG_LOG\n- $line"
+done <<< $(git --no-pager log --no-merges --format=%s | awk '/^Set version to '"${VERSION}"'$/{flag=1;next}/^Set version to [0-9]+\.[0-9]+\.[0-9]+$/{flag=0}flag')
+
+COMMITS=$(printf "%s<br>" "${CHG_LOG[@]}")
 
 
+git checkout -b ${VERSION}
+echo $VERSION > version
+git add update_version.sh
+git add version
+git commit -m "Set version to $VERSION"
+git push --set-upstream origin ${VERSION}
 
-
-
-
-#CHG_LOG=$(git log --no-merges --format=%s | awk '/^Set version to '${VERSION}'$/{flag=1;next}/^Set version to [0-9]+\.[0-9]+\.[0-9]+$/{flag=0}flag' | awk '{key=$0; getline; print key " " $0;}')
-#CHG_LOG=$(git log --no-merges --format=%s | sed -e '/^Set version to '${VERSION}'$/{flag=1;next}/^Set version to [0-9]+\.[0-9]+\.[0-9]+$/{flag=0}flag')
-
-echo ${CHG_LOG}
-
-exit 0
-#git checkout -b ${VERSION}
-#echo $VERSION > version
-#git add update_version.sh
-#git add version
-#git commit -m "Set version to $VERSION"
-##git push origin
-#git push --set-upstream origin ${VERSION}
-
-#API_JSON=$(printf '{"title": "%s", "body": "%s", "head": "%s:%s", "base": "master"}' $VERSION $CHG_LOG $REPOSITORY $VERSION)
-API_JSON=$(printf '{"title": "%s", "body": "%s", "head": "%s:%s", "base": "master"}' $VERSION 'mytest' $REPOSITORY $VERSION)
-echo curl -H "Authorization: token ${TOKEN}" -X POST --data "$API_JSON" https://api.github.com/repos/${OWNER}/${REPOSITORY}/pulls
-#echo ${API_JSON}
+API_JSON=$(printf '{"title": "%s", "body": "%s", "head": "%s:%s", "base": "master"}' $VERSION "$COMMITS" $REPOSITORY $VERSION)
+echo $API_JSON
+curl -H "Authorization: token ${TOKEN}" -X POST --data "$API_JSON" https://api.github.com/repos/${OWNER}/${REPOSITORY}/pulls
